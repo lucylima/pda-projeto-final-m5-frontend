c3cbdbed58a53af4b107abedd6db2857c27f4fbe
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-03 12:07:37 -0300

    feat: bit post template added

---
+++ src/components/BitPost/BitPost.jsx
@@ -0,0 +1,31 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+
+function BitPost() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export { BitPost };
