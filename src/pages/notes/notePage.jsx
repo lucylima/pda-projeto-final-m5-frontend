62e1f07bef64510905387d04a075e07217f9f719
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-03 11:57:17 -0300

    feat: added header and bottom navigation

--- src/pages/notes/notePage.jsx
+++ src/pages/notes/notePage.jsx
@@ -1,11 +1,14 @@
 import Box from "@mui/material/Box";
+import { Header } from "../../components/Header/Header.jsx";
+import { Navigation } from "../../components/Navigation/Navigation.jsx";
 
 function NotePage() {
-  return(
-    <Box>
-      box actisd
-    </Box>
-  )
+  return (
+    <>
+      <Header />
+      <Navigation />
+    </>
+  );
 }
 
 export { NotePage };
