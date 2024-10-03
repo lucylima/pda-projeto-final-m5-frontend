3aa8718bde663c30f9296d1beba145f0f8352ccf
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-03 10:17:00 -0300

    feat: login page mounted

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,24 +1,57 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 function LoginPage() {
   return (
-    <>
-      <h1>BlissFields</h1>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h4" component="h3" sx={{ textAlign: "center" }}>
+        BlissFields
+      </Typography>
       <Box
         component="form"
-        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+        sx={{
+          "& > :not(style)": {
+            m: 1,
+            width: "25ch",
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          },
+        }}
         noValidate
         autoComplete="off"
       >
-        <h2>Login</h2>
-
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
+        <Typography variant="h5" component="h2" sx={{ textAlign: "center" }}>
+          login
+        </Typography>
+        <TextField
+          id="email-field"
+          label="Email"
+          variant="standard"
+          type="email"
+        />
+        <TextField
+          id="password-field"
+          label="Senha"
+          variant="standard"
+          type="password"
+        />
         <Button variant="contained">Entrar</Button>
       </Box>
-    </>
+    </Container>
   );
 }
 
