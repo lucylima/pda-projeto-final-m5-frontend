600f606f344b554d73d36453e01b3f6fdc65570b
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-03 11:13:33 -0300

    feat: sign up page done

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,24 +1,91 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useState } from "react";
 
 function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // axios post
+  };
+
   return (
     <>
-      <h1>BlissFields</h1>
-      <Box
-        component="form"
-        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
-        noValidate
-        autoComplete="off"
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+        }}
       >
-        <h2>Cadastre-se</h2>
+        <Typography variant="h4" component="h3" sx={{ textAlign: "center" }}>
+          BlissFields
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": {
+              m: 1,
+              width: "25ch",
+              mx: "auto",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Typography variant="h5" component="h2" sx={{ textAlign: "center" }}>
+            Cadastre-se
+          </Typography>
 
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <Button variant="contained">Entrar</Button>
-      </Box>
+          <TextField
+            id="username-field"
+            label="Nome de usuário"
+            variant="standard"
+            type="text"
+            onChange={handleUsernameChange}
+          />
+
+          <TextField
+            id="email-field"
+            label="Email"
+            variant="standard"
+            type="email"
+            onChange={handleEmailChange}
+          />
+
+          <TextField
+            id="password-field"
+            label="Senha"
+            variant="standard"
+            type="password"
+            onChange={handlePasswordChange}
+          />
+          <Button variant="contained" onClick={handleSubmit}>
+            Entrar
+          </Button>
+        </Box>
+      </Container>
     </>
   );
 }
