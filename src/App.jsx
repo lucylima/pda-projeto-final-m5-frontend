18b9159cc0458ad179b3a28da0e9194068b3dc72
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-05 14:45:54 -0300

    cleanup: removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { LoginPage } from "./pages/login/LoginPage.jsx";
 import { RegisterPage } from "./pages/register/RegisterPage.jsx";
-import { Header } from "./components/Header/Header.jsx";
+import { BlissFieldsPage } from "./pages/blissFields/BlissFieldsPage.jsx";
+import { MoodChartsPage } from "./pages/moodcharts/MoodChartsPage.jsx";
+import { NotePage } from "./pages/notes/NotePage.jsx";
+
 function App() {
   return (
     <>
-      {/* <LoginPage /> */}
-      <Header />
-      {/* <RegisterPage></RegisterPage> */}
+      <NotePage />
     </>
   );
 }
