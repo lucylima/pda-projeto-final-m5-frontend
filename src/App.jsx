9c5e657341e73e763ed58824ff56d2e072b87df0
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-07 11:40:10 -0300

    feat: theme provider for darkmode, header and navigation mounted

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import { RegisterPage } from "./pages/register/RegisterPage.jsx";
 import { BlissFieldsPage } from "./pages/blissFields/BlissFieldsPage.jsx";
 import { MoodChartsPage } from "./pages/moodcharts/MoodChartsPage.jsx";
 import { NotePage } from "./pages/notes/NotePage.jsx";
+import { Navigation } from "./components/Navigation/Navigation.jsx";
+import { Header } from "./components/Header/Header.jsx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 function App() {
   return (
     <>
-      <NotePage />
+      <ThemeProvider theme={theme}> 
+        <Header />
+        <NotePage />
+        <Navigation />
+      </ThemeProvider>
     </>
   );
 }
