3937341d35f769f3ce57aad8008aba1f0e107ec3
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-12 14:52:01 -0300

    feat: adding providers and css baseline from mui

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,21 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "./index.css";
-import CssBaseline from '@mui/material/CssBaseline';
+import "./index.css"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BitProvider } from "./context/bitsContext.jsx";
+import { NoteProvider } from "./context/noteContext.jsx";
 import App from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CssBaseline />
-    <App />
+    <NoteProvider>
+      <BitProvider>
+        <App />
+      </BitProvider>
+    </NoteProvider>
   </StrictMode>
 );
