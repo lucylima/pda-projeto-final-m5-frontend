5dbab16eb2a928dcc914e8b075d3beede23921a3
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-12 16:28:08 -0300

    feat: added router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "./index.css"
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BitProvider } from "./context/bitsContext.jsx";
 import { NoteProvider } from "./context/noteContext.jsx";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import Router from "./router/router.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
 
 createRoot(document.getElementById("root")).render(
@@ -15,7 +16,7 @@ createRoot(document.getElementById("root")).render(
     <CssBaseline />
     <NoteProvider>
       <BitProvider>
-        <App />
+        <RouterProvider router={Router} />
       </BitProvider>
     </NoteProvider>
   </StrictMode>
