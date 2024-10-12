eff8dbde2e31dcc574023e6431b8e430733d0f3c
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-12 15:38:49 -0300

    feat: note fetch working and correct mapping

--- src/pages/notes/notePage.jsx
+++ src/pages/notes/notePage.jsx
@@ -1,53 +1,30 @@
 import AddIcon from "@mui/icons-material/Add";
 import Container from "@mui/material/Container";
 import Fab from "@mui/material/Fab";
-import { useState } from "react";
+import { Header } from "../../components/Header/Header.jsx";
+import { useContext, useEffect, useState } from "react";
 import { Navigation } from "../../components/Navigation/Navigation.jsx";
 import { Note } from "../../components/Note/Note.jsx";
 import { NewNoteModal } from "../../components/NewNoteModal/NewNoteModal.jsx";
+import { NoteContext } from "../../context/noteContext.jsx";
+import axios from "axios";
 
 function NotePage() {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      title: "Lucy primeira nota",
-      text: "bobeira kkkk",
-      mood: "Surpresa",
-      timestamp: "05/10/24",
-    },
-    {
-      id: 2,
-      title: "colinas de pirajá",
-      text: "bobeira kkkk",
-      mood: "Surpresa",
-      timestamp: "05/10/24",
-    },
-    {
-      id: 3,
-      title: "colinas de pirajá",
-      text: "bobeira kkkk",
-      mood: "Surpresa",
-      timestamp: "05/10/24",
-    },
-    {
-      id: 4,
-      title: "colinas de pirajá",
-      text: "bobeira kkkk",
-      mood: "Surpresa",
-      timestamp: "05/10/24",
-    },
-    {
-      id: 5,
-      title: "colinas de pirajá",
-      text: "bobeira kkkk",
-      mood: "Surpresa",
-      timestamp: "05/10/24",
-    },
-  ]);
+  const { note, setNote } = useContext(NoteContext);
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    const getNotes = async () => {
+      const { data } = await axios.get(
+        "https://api-blissfields.onrender.com/notes/6ea827a4-f250-45bc-81e1-1416c09e415b"
+      );
+      setNote([...note, ...data.notes]);
+    };
+    getNotes();
+  }, []);
 
   return (
     <>
@@ -65,16 +42,16 @@ function NotePage() {
           gap: 2,
         }}
       >
-        {notes.map((note) => {
+        {note.map((note) => {
           return (
             <Note
-              key={note.id}
+              key={note.notes_id}
               note={{
-                id: note.id,
+                id: note.notes_id,
                 title: note.title,
                 text: note.text,
                 mood: note.mood,
-                timestamp: note.timestamp,
+                timestamp: note.createdAt,
               }}
             />
           );
