6e2cf98de07289f52e56023738f48f7288a28784
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-13 22:58:47 -0300

    cleanup: removed test bit

--- src/context/bitsContext.jsx
+++ src/context/bitsContext.jsx
@@ -3,16 +3,7 @@ import { createContext, useState } from "react";
 const BitContext = createContext();
 
 function BitProvider({ children }) {
-  const [bit, setBit] = useState([
-    {
-      bits_id: "bac52b91",
-      text: "Hoje estou triste demais!",
-      createdAt: "2024-10-08T23:30:08.900Z",
-      User: {
-        username: "Teste2",
-      },
-    },
-  ]);
+  const [bit, setBit] = useState([]);
   return (
     <BitContext.Provider value={{ bit, setBit }}>
       {children}
